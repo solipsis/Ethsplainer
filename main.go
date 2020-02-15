8e107ffe2e5374e8bb447beaa29d6d7f1c6689a8
peterhendrick <peterjhendrick@gmail.com>
2020-02-14 21:36:34 -0700

    Removed CORS restrictions on server

--- main.go
+++ main.go
@@ -53,6 +53,9 @@ func main() {
 }
 
 func handleData(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 
 	dec := json.NewDecoder(r.Body)
 	req := request{}
